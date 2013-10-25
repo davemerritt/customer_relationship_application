cc73014b257d72f2ad06be530a288a15178a1e44
davemerritt <davidmerritt6@gmail.com>
2013-10-25 13:06:29 -0400

    Contact class

--- crm.rb
+++ crm.rb
@@ -40,7 +40,30 @@ class CRM
 		end
 	end
 end
+#contact class for collecting and storing contact information.
+class Contact
+	def initialize(first_name, last_name, email, note)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@note = note
+	end
+#Requests information. Still needs to store it somewhere.
+	def add_new
+		print "First Name?"
+		first_name = gets.chomp
+		print "Last Name?"
+		last_name = gets.chomp
+		print "Email Address?"
+		email = gets.chomp
+		print "Add a note: "
+		note = gets.chomp
+		contact = Contact.new(first_name, last_name, email, note)
+	end
+end
+
 #calling new instance of the class, and the method 
 #on the instance.
 crm = CRM.new(My crm)
 crm.print_menu
+
